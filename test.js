66482142a748d317ff5a8983e04a2417ba109687
Salim <salimbhandi07@gmail.com>
2024-09-20 11:43:48 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,63 @@
+// const arr = [2, 4, 5, 6, 7, 5];
+
+// const uniqeArray = Array.from(new Set(arr));
+// const largeNumber = arr.sort((a, b) => b - a);
+// const assending = arr.sort((a, b) => b - a);
+// console.log("largeNumber", largeNumber[1]);
+// console.log("largeNumber", largeNumber);
+// -----------------------------------------------------------------
+
+// const uniqueArr = [...new Set(arr)];
+// console.log(uniqeArray);
+// -----------------------------------------------------------------
+
+// let str = "my is j in log name12312";
+
+// const words = str.split(" ");
+
+// let wordsCount = 0;
+// let largestStr = "";
+
+// words.forEach((word) => {
+//   if (word.length > wordsCount) {
+//     wordsCount = word.length;
+//     largestStr = word;
+//   }
+// });
+
+// console.log("word", largestStr);
+// -----------------------------------------------------------------
+
+// const isPalindrome = (str) => {
+//   let cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+//   let reverString = cleanedStr.split("").reverse().join("");
+//   return cleanedStr == reverString;
+// };
+
+// let testStr = "A man, a plan, a canal, Panama";
+
+// console.log("isPalindrome", isPalindrome(testStr));
+
+// -----------------------------------------------------------------
+
+//largest
+//secound largest
+//unique
+//remove duplicate
+//asscending
+//desending
+//Find largest word in string
+//find isPalindrome or not
+
+// function isPalindrome(str) {
+//   let cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+//   let reversedStr = cleanedStr.split("").reverse().join("");
+//   console.log("reversedStr", reversedStr);
+
+//   return cleanedStr === reversedStr;
+// }
+
+// let testStr = "A man, a plan, a canal, Panama";
+// console.log(isPalindrome(testStr));
+
+//   let cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
